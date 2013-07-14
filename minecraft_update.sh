e08f110b33d927e06e289fc4e794c7cf3105ad69
Ihara Hiromasa <iharahiromasa@gmail.com>
2013-07-14 18:26:16 +0900

    Update minecraft_update.sh

--- minecraft_update.sh
+++ minecraft_update.sh
@@ -12,14 +12,20 @@ fi
 
 #compare
 cmp minecraft_server.jar /opt/minecraft/minecraft_server.jar
+
 #if not difference
 if [ $? == 0 ] ; then
   exit 0
 fi
+
 #if error
 if [ $? == 2 ] ; then
   exit 1
 fi
+
+#replace
 cp minecraft_server.jar /opt/minecraft/
+
+#restart
 service minecraft stop
 service minecraft start
